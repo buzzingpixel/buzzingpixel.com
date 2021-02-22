a942d7315838903563b8dab829ad6cc9d3672a99
TJ Draper <tj@buzzingpixel.com>
2021-02-21 23:10:13 -0600

    Add Ansel Craft Field Type Usage Documentation

--- config/Routes/index.php
+++ config/Routes/index.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use App\Http\Response\Home\HomeAction;
 use App\Http\Response\Software\AnselCraft\AnselCraftAction;
 use App\Http\Response\Software\AnselCraft\Documentation\AnselCraftDocFieldTypeSettingsAction;
+use App\Http\Response\Software\AnselCraft\Documentation\AnselCraftDocFieldTypeUseAction;
 use App\Http\Response\Software\AnselCraft\Documentation\AnselCraftDocIndexAction;
 use App\Http\Response\Software\SoftwareAction;
 use Slim\App;
@@ -15,4 +16,5 @@ return static function (App $app): void {
     $app->get('/software/ansel-craft', AnselCraftAction::class);
     $app->get('/software/ansel-craft/documentation', AnselCraftDocIndexAction::class);
     $app->get('/software/ansel-craft/documentation/field-type-settings', AnselCraftDocFieldTypeSettingsAction::class);
+    $app->get('/software/ansel-craft/documentation/field-type-use', AnselCraftDocFieldTypeUseAction::class);
 };
