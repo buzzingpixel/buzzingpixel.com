ac04f109d2f1d6da9fff6bcaf43ed78f2dc974d5
TJ Draper <tj@buzzingpixel.com>
2021-03-06 23:31:08 -0600

    Fix issues with migrations

--- config/Data/Migrations/Version20210307052745.php
+++ config/Data/Migrations/Version20210307052745.php
@@ -7,7 +7,7 @@ namespace Config\Data\Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20210306055533 extends AbstractMigration
+final class Version20210307052745 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -16,8 +16,7 @@ final class Version20210306055533 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE user_password_reset_tokens (id UUID NOT NULL, user_id UUID NOT NULL, created_at TIMESTAMP(0) WITH TIME ZONE NOT NULL, PRIMARY KEY(id, user_id))');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_F963EF68BF396750 ON user_password_reset_tokens (id)');
+        $this->addSql('CREATE TABLE user_password_reset_tokens (id UUID NOT NULL, user_id UUID NOT NULL, created_at TIMESTAMP(0) WITH TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN user_password_reset_tokens.id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN user_password_reset_tokens.user_id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN user_password_reset_tokens.created_at IS \'(DC2Type:datetimetz_immutable)\'');
