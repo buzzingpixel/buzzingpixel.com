75507f549210e2a01df68dc4ac04e30fc97ac5da
TJ Draper <tj@buzzingpixel.com>
2021-03-25 22:30:37 -0500

    Continue Account Profile Page

---
+++ assets/js/Components/Selects.js
@@ -0,0 +1,32 @@
+/**
+ * @see https://joshuajohnson.co.uk/Choices/
+ * @see https://github.com/jshjohnson/Choices
+ */
+
+import Loader from '../Helpers/Loader.js';
+
+class Selects {
+    /**
+     * @param {NodeList} els
+     */
+    constructor (els) {
+        Loader.loadCss(
+            'https://cdn.jsdelivr.net/npm/choices.js@9.0.1/public/assets/styles/choices.min.css',
+        );
+
+        Loader.loadJs('https://cdn.jsdelivr.net/npm/choices.js@9.0.1/public/assets/scripts/choices.min.js').then(() => {
+            els.forEach((el) => {
+                // eslint-disable-next-line no-undef,no-new
+                new Choices(el, {
+                    removeItemButton: true,
+                    shouldSort: false,
+                    classNames: {
+                        containerInner: 'inline-block align-top p-2 border overflow-hidden max-w-lg block w-full shadow-sm focus:ring-meteor focus:border-meteor sm:max-w-xs sm:text-sm border-gray-300 rounded-md',
+                    },
+                });
+            });
+        });
+    }
+}
+
+export default Selects;
--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,7 @@ import SetGlobalData from './SetUp/SetGlobalData.js';
 // Components
 import MainNav from './Components/MainNav.js';
 import PrismCodeHighlighting from './Components/PrismCodeHighlighting.js';
+import Selects from './Components/Selects.js';
 
 /**
  * Run
@@ -32,4 +33,10 @@ if (document.querySelector('code')
     || document.querySelector('pre')
 ) {
     new PrismCodeHighlighting();
+}
+
+// Selects
+const selectEls = document.querySelectorAll('[ref="select"]');
+if (selectEls.length > 0) {
+    new Selects(selectEls);
 }
