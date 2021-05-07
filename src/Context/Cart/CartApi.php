f5ac0f800cdfe59c0d64829fb3f72f668a7bad16
TJ Draper <tj@buzzingpixel.com>
2021-05-07 08:34:19 -0500

    Add saveCart method to CartApi

--- src/Context/Cart/CartApi.php
+++ src/Context/Cart/CartApi.php
@@ -6,16 +6,24 @@ namespace App\Context\Cart;
 
 use App\Context\Cart\Entities\Cart;
 use App\Context\Cart\Services\FetchCurrentUserCart;
+use App\Context\Cart\Services\SaveCart;
+use App\Payload\Payload;
 
 class CartApi
 {
     public function __construct(
         private FetchCurrentUserCart $fetchCurrentUserCart,
+        private SaveCart $saveCart,
     ) {
     }
 
     public function fetchCurrentUserCart(): Cart
     {
         return $this->fetchCurrentUserCart->fetch();
+    }
+
+    public function saveCart(Cart $cart): Payload
+    {
+        return $this->saveCart->save($cart);
     }
 }
