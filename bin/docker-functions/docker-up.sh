f4e7b3ebeacfde4f20d742a800a780ea05ebbe97
TJ Draper <tj@buzzingpixel.com>
2021-05-10 09:06:20 -0500

    Refactor to move Traefik to an external independent configuration

--- bin/docker-functions/docker-up.sh
+++ bin/docker-functions/docker-up.sh
@@ -5,35 +5,6 @@ function docker-up-help() {
 }
 
 function docker-up() {
-    # Make sure requirements are met
-    if [ ! $(command -v mkcert) ]; then
-        printf "${Red}'mkcert' must be installed. To install mkcert, run the following: ${Reset}\n";
-        printf "${Cyan}(homebrew is required)\n\n";
-        printf "${Green}    brew install mkcert\n";
-        printf "    brew install nss\n";
-        printf "    mkcert -install${Reset}\n\n";
-        printf "${Red}Halting docker-up ${Reset}\n";
-
-        return 1;
-    fi
-
-    # Local, reusable function to make certs
-    function localMkCert() {
-        # Only run if our cert or key is missing
-        printf "${Cyan}Generating new local cert for ${1} with mkcert...${Reset}\n";
-
-        mkcert \
-            -cert-file docker/application/certs/${1}.cert \
-            -key-file docker/application/certs/${1}.key \
-            ${1};
-
-        printf "${Green}${1} cert created${Reset}\n";
-    }
-
-    # Ensure certificates exist with or reusable local function
-    localMkCert "buzzingpixel.localtest.me";
-    localMkCert "localhost";
-
     chmod -R 0777 storage;
 
     docker compose ${composeFiles} -p buzzingpixel up -d;
