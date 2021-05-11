5e34410fc110b010b5dc8a0d4730393c62dd7e4c
TJ Draper <tj@buzzingpixel.com>
2021-05-10 22:47:28 -0500

    Fix a migrations issue

--- config/Data/Migrations/Version20210427034036.php
+++ config/Data/Migrations/Version20210427034036.php
@@ -17,14 +17,12 @@ final class Version20210427034036 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE TABLE cart (id UUID NOT NULL, user_id UUID DEFAULT NULL, last_touched_at TIMESTAMP(0) WITH TIME ZONE NOT NULL, created_at TIMESTAMP(0) WITH TIME ZONE NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_BA388B7A76ED395 ON cart (user_id)');
         $this->addSql('COMMENT ON COLUMN cart.id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN cart.user_id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN cart.last_touched_at IS \'(DC2Type:datetimetz_immutable)\'');
         $this->addSql('COMMENT ON COLUMN cart.created_at IS \'(DC2Type:datetimetz_immutable)\'');
         $this->addSql('CREATE TABLE cart_items (id UUID NOT NULL, cart_id UUID DEFAULT NULL, softare_id UUID DEFAULT NULL, quantity INT NOT NULL, slug VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_BEF484451AD5CDBF ON cart_items (cart_id)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_BEF48445A6CF31DF ON cart_items (softare_id)');
         $this->addSql('COMMENT ON COLUMN cart_items.id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN cart_items.cart_id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN cart_items.softare_id IS \'(DC2Type:uuid)\'');
@@ -35,7 +33,6 @@ final class Version20210427034036 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE cart_items DROP CONSTRAINT FK_BEF484451AD5CDBF');
         $this->addSql('DROP TABLE cart');
         $this->addSql('DROP TABLE cart_items');
     }
