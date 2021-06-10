db4ff9cb2e06ad1f858acbf2e002946dbc401f39
TJ Draper <tj@buzzingpixel.com>
2021-06-10 09:05:44 -0500

    Fix an occasional issue with Docker that throws an exception when trying to delete tmp files

--- docker/utility/prod.sh
+++ docker/utility/prod.sh
@@ -7,6 +7,11 @@ touch /log-volume/nginx/error.log;
 chmod -R 0777 /log-volume;
 
 while true; do
+    chmod -R 0777 /tmp;
+    sleep 5;
+done
+
+while true; do
     chmod -R 0777 /storage-volume;
     sleep 120;
 done
