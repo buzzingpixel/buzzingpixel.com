90fc7851669a422cca995b543ea33231dbce7b90
TJ Draper <tj@buzzingpixel.com>
2021-07-23 17:43:40 -0500

    Add cart api methods to clear cart

--- src/Context/Cart/CartApi.php
+++ src/Context/Cart/CartApi.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Context\Cart;
 
 use App\Context\Cart\Entities\Cart;
+use App\Context\Cart\Services\ClearCart;
 use App\Context\Cart\Services\FetchCurrentUserCart;
 use App\Context\Cart\Services\SaveCart;
 use App\Payload\Payload;
@@ -12,8 +13,9 @@ use App\Payload\Payload;
 class CartApi
 {
     public function __construct(
-        private FetchCurrentUserCart $fetchCurrentUserCart,
         private SaveCart $saveCart,
+        private ClearCart $clearCart,
+        private FetchCurrentUserCart $fetchCurrentUserCart,
     ) {
     }
 
@@ -25,5 +27,15 @@ class CartApi
     public function saveCart(Cart $cart): Payload
     {
         return $this->saveCart->save($cart);
+    }
+
+    public function clearCart(Cart $cart): void
+    {
+        $this->clearCart->clear($cart);
+    }
+
+    public function clearCurrentUserCart(): void
+    {
+        $this->clearCart->clear($this->fetchCurrentUserCart());
     }
 }
