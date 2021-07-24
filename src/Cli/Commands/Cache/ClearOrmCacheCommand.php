0567c71d9bb4d374218ed489e7a5c639e5b75703
TJ Draper <tj@buzzingpixel.com>
2021-07-24 10:41:11 -0500

    Update the clear ORM cache command

--- src/Cli/Commands/Cache/ClearOrmCacheCommand.php
+++ src/Cli/Commands/Cache/ClearOrmCacheCommand.php
@@ -5,16 +5,22 @@ declare(strict_types=1);
 namespace App\Cli\Commands\Cache;
 
 use Config\General;
+use Doctrine\Common\Cache\ApcCache;
+use Doctrine\Common\Cache\XcacheCache;
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Throwable;
 
 use function exec;
 
 class ClearOrmCacheCommand extends Command
 {
-    public function __construct(private General $config)
-    {
+    public function __construct(
+        private General $config,
+        private EntityManager $entityManager,
+    ) {
         parent::__construct();
     }
 
@@ -34,6 +40,114 @@ class ClearOrmCacheCommand extends Command
         $ormCachePath = $storagePath . '/doctrine/*';
 
         exec(command: 'rm -rf ' . $ormCachePath);
+
+        /**
+         * Clear entity manager metadata cache
+         *
+         * @see \Doctrine\ORM\Tools\Console\Command\ClearCache\MetadataCommand
+         * ^ The damn thing is so locked down we can't use this command from the
+         * outside at all
+         */
+        try {
+            $metadataCacheDriver = $this->entityManager
+                ->getConfiguration()
+                ->getMetadataCacheImpl();
+
+            $instanceOfApcCache = $metadataCacheDriver instanceof ApcCache;
+
+            $instanceOfXCache = $metadataCacheDriver instanceof XcacheCache;
+
+            if (
+                $metadataCacheDriver !== null &&
+                ! $instanceOfApcCache &&
+                ! $instanceOfXCache
+            ) {
+                /**
+                 * @psalm-suppress UndefinedInterfaceMethod
+                 * @phpstan-ignore-next-line
+                 */
+                $metadataCacheDriver->deleteAll();
+
+                /**
+                 * @psalm-suppress UndefinedInterfaceMethod
+                 * @phpstan-ignore-next-line
+                 */
+                $metadataCacheDriver->flushAll();
+            }
+        } catch (Throwable $e) {
+        }
+
+        /**
+         * Clear entity manager query cache
+         *
+         * @see \Doctrine\ORM\Tools\Console\Command\ClearCache\QueryCommand
+         * ^ The damn thing is so locked down we can't use this command from the
+         * outside at all
+         */
+        try {
+            $queryCacheDriver = $this->entityManager
+                ->getConfiguration()
+                ->getMetadataCacheImpl();
+
+            $instanceOfApcCache = $queryCacheDriver instanceof ApcCache;
+
+            $instanceOfXCache = $queryCacheDriver instanceof XcacheCache;
+
+            if (
+                $queryCacheDriver !== null &&
+                ! $instanceOfApcCache &&
+                ! $instanceOfXCache
+            ) {
+                /**
+                 * @psalm-suppress UndefinedInterfaceMethod
+                 * @phpstan-ignore-next-line
+                 */
+                $queryCacheDriver->deleteAll();
+
+                /**
+                 * @psalm-suppress UndefinedInterfaceMethod
+                 * @phpstan-ignore-next-line
+                 */
+                $queryCacheDriver->flushAll();
+            }
+        } catch (Throwable $e) {
+        }
+
+        /**
+         * Clear entity manager query cache
+         *
+         * @see \Doctrine\ORM\Tools\Console\Command\ClearCache\QueryCommand
+         * ^ The damn thing is so locked down we can't use this command from the
+         * outside at all
+         */
+        try {
+            $resultCacheDriver = $this->entityManager
+                ->getConfiguration()
+                ->getMetadataCacheImpl();
+
+            $instanceOfApcCache = $resultCacheDriver instanceof ApcCache;
+
+            $instanceOfXCache = $resultCacheDriver instanceof XcacheCache;
+
+            if (
+                $resultCacheDriver !== null &&
+                ! $instanceOfApcCache &&
+                ! $instanceOfXCache
+            ) {
+                /**
+                 * @psalm-suppress UndefinedInterfaceMethod
+                 * @phpstan-ignore-next-line
+                 */
+                $resultCacheDriver->deleteAll();
+
+                /**
+                 * @psalm-suppress UndefinedInterfaceMethod
+                 * @phpstan-ignore-next-line
+                 */
+                $resultCacheDriver->flushAll();
+            }
+        } catch (Throwable $e) {
+        }
 
         $output->writeln(messages: '<fg=green>ORM cache cleared</>');
 
