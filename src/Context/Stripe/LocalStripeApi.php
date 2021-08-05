8e23453cdf16d2cd1b5b3acee08cc43fb5fbe61f
TJ Draper <tj@buzzingpixel.com>
2021-08-05 10:08:17 -0500

    Add methods to LocalStripeApi

--- src/Context/Stripe/LocalStripeApi.php
+++ src/Context/Stripe/LocalStripeApi.php
@@ -5,8 +5,22 @@ declare(strict_types=1);
 namespace App\Context\Stripe;
 
 use App\Context\Cart\Entities\Cart;
+use App\Context\Stripe\Entities\StripeCustomerCollection;
+use App\Context\Stripe\Entities\StripeInvoiceCollection;
+use App\Context\Stripe\Entities\StripeInvoiceItemCollection;
+use App\Context\Stripe\Entities\StripePriceCollection;
+use App\Context\Stripe\Entities\StripeProductCollection;
+use App\Context\Stripe\Entities\StripeSubscriptionCollection;
+use App\Context\Stripe\Entities\StripeTaxRateCollection;
 use App\Context\Stripe\Services\CreateBillingPortalSession;
 use App\Context\Stripe\Services\CreateCheckoutSession;
+use App\Context\Stripe\Services\StripeFetchCustomers;
+use App\Context\Stripe\Services\StripeFetchInvoiceItems;
+use App\Context\Stripe\Services\StripeFetchInvoices;
+use App\Context\Stripe\Services\StripeFetchPrices;
+use App\Context\Stripe\Services\StripeFetchProducts;
+use App\Context\Stripe\Services\StripeFetchSubscriptions;
+use App\Context\Stripe\Services\StripeFetchTaxRates;
 use App\Context\Stripe\Services\SyncCustomers;
 use App\Context\Stripe\Services\SyncLicenses;
 use App\Context\Stripe\Services\SyncProducts;
@@ -20,7 +34,14 @@ class LocalStripeApi
         private SyncProducts $syncProducts,
         private SyncLicenses $syncLicenses,
         private SyncCustomers $syncCustomers,
+        private StripeFetchPrices $fetchPrices,
+        private StripeFetchInvoices $fetchInvoices,
+        private StripeFetchProducts $fetchProducts,
+        private StripeFetchTaxRates $fetchTaxRates,
+        private StripeFetchCustomers $fetchCustomers,
+        private StripeFetchInvoiceItems $fetchInvoiceItems,
         private CreateCheckoutSession $createCheckoutSession,
+        private StripeFetchSubscriptions $fetchSubscriptions,
         private CreateBillingPortalSession $createBillingPortalSession,
     ) {
     }
@@ -48,5 +69,76 @@ class LocalStripeApi
     public function createCheckoutSession(Cart $cart): CheckoutSession
     {
         return $this->createCheckoutSession->create($cart);
+    }
+
+    /**
+     * @param mixed[] $params
+     *
+     * @phpstan-ignore-next-line
+     */
+    public function fetchCustomers(array $params = []): StripeCustomerCollection
+    {
+        return $this->fetchCustomers->fetch($params);
+    }
+
+    /**
+     * @param mixed[] $params
+     *
+     * @phpstan-ignore-next-line
+     */
+    public function fetchInvoiceItems(array $params = []): StripeInvoiceItemCollection
+    {
+        return $this->fetchInvoiceItems->fetch($params);
+    }
+
+    /**
+     * @param mixed[] $params
+     *
+     * @phpstan-ignore-next-line
+     */
+    public function fetchInvoices(array $params = []): StripeInvoiceCollection
+    {
+        return $this->fetchInvoices->fetch($params);
+    }
+
+    /**
+     * @param mixed[] $params
+     *
+     * @phpstan-ignore-next-line
+     */
+    public function fetchPrices(array $params = []): StripePriceCollection
+    {
+        return $this->fetchPrices->fetch($params);
+    }
+
+    /**
+     * @param mixed[] $params
+     *
+     * @phpstan-ignore-next-line
+     */
+    public function fetchProducts(array $params = []): StripeProductCollection
+    {
+        return $this->fetchProducts->fetch($params);
+    }
+
+    /**
+     * @param mixed[] $params
+     *
+     * @phpstan-ignore-next-line
+     */
+    public function fetchSubscriptions(
+        array $params = []
+    ): StripeSubscriptionCollection {
+        return $this->fetchSubscriptions->fetch($params);
+    }
+
+    /**
+     * @param mixed[] $params
+     *
+     * @phpstan-ignore-next-line
+     */
+    public function fetchTaxRates(array $params = []): StripeTaxRateCollection
+    {
+        return $this->fetchTaxRates->fetch($params);
     }
 }
