720170942d3b8a6ce43245fedff1846b33561b7d
TJ Draper <tj@buzzingpixel.com>
2021-08-10 10:12:13 -0500

    Update date time display on orders to use user's timezone

--- src/Http/Response/Account/Purchases/AccountPurchasesDetailAction.php
+++ src/Http/Response/Account/Purchases/AccountPurchasesDetailAction.php
@@ -10,11 +10,14 @@ use App\Context\Users\Entities\LoggedInUser;
 use App\Http\Entities\Meta;
 use App\Persistence\QueryBuilders\Orders\OrderQueryBuilder;
 use Config\General;
+use DateTimeImmutable;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Exception\HttpNotFoundException;
 use Twig\Environment as TwigEnvironment;
+
+use function assert;
 
 class AccountPurchasesDetailAction
 {
@@ -49,90 +52,98 @@ class AccountPurchasesDetailAction
 
         $response = $this->responseFactory->createResponse();
 
+        $orderDate = $order->orderDate();
+
+        assert($orderDate instanceof DateTimeImmutable);
+
         /** @noinspection PhpUnhandledExceptionInspection */
-        $response->getBody()->write(
-            string: $this->twig->render(
-                name: '@app/Http/Response/Account/AccountKeyValuePage.twig',
-                context: [
-                    'meta' => new Meta(
+        $response->getBody()->write($this->twig->render(
+            '@app/Http/Response/Account/AccountKeyValuePage.twig',
+            [
+                'meta' => new Meta(
                     metaTitle: 'Viewing Order | Purchases | Account',
-                    ),
-                    'accountMenu' => $accountMenu,
-                    'breadcrumbSingle' => [
+                ),
+                'accountMenu' => $accountMenu,
+                'breadcrumbSingle' => [
+                    'content' => 'Purchases',
+                    'uri' => '/account/purchases',
+                ],
+                'breadcrumbTrail' => [
+                    [
+                        'content' => 'Account',
+                        'uri' => '/account',
+                    ],
+                    [
                         'content' => 'Purchases',
                         'uri' => '/account/purchases',
                     ],
-                    'breadcrumbTrail' => [
+                    ['content' => 'View'],
+                ],
+                'keyValueCard' => [
+                    'headline' => 'Order ID: ' . $order->id(),
+                    'subHeadline' => 'Order Total: ' . $order->totalFormatted(),
+                    'items' => [
                         [
-                            'content' => 'Account',
-                            'uri' => '/account',
+                            'key' => 'Order ID',
+                            'value' => $order->id(),
                         ],
                         [
-                            'content' => 'Purchases',
-                            'uri' => '/account/purchases',
+                            'key' => 'Order Date',
+                            'value' => $orderDate->setTimezone(
+                                $this->loggedInUser->user()->timezone(),
+                            )->format('F jS, Y, g:i a'),
                         ],
-                        ['content' => 'View'],
-                    ],
-                    'keyValueCard' => [
-                        'headline' => 'Order ID: ' . $order->id(),
-                        'subHeadline' => 'Order Total: ' . $order->totalFormatted(),
-                        'items' => [
-                            [
-                                'key' => 'Order ID',
-                                'value' => $order->id(),
-                            ],
-                            [
-                                'key' => 'Subtotal',
-                                'value' => $order->subTotalFormatted(),
-                            ],
-                            [
-                                'key' => 'Tax',
-                                'value' => $order->taxFormatted(),
-                            ],
-                            [
-                                'key' => 'Total',
-                                'value' => $order->totalFormatted(),
-                            ],
-                            [
-                                'template' => 'Http/_Infrastructure/Display/SimpleTableList.twig',
-                                'key' => $order->orderItems()->count() > 1 ?
-                                    'Order Items' :
-                                    'Order Item',
-                                'value' => [
-                                    'items' => $order->orderItems()->mapToArray(
-                                        static function (OrderItem $item): array {
-                                            $content = $item->software()->name();
+                        [
+                            'key' => 'Subtotal',
+                            'value' => $order->subTotalFormatted(),
+                        ],
+                        [
+                            'key' => 'Tax',
+                            'value' => $order->taxFormatted(),
+                        ],
+                        [
+                            'key' => 'Total',
+                            'value' => $order->totalFormatted(),
+                        ],
+                        [
+                            'template' => 'Http/_Infrastructure/Display/SimpleTableList.twig',
+                            'key' => $order->orderItems()->count() > 1 ?
+                                'Order Items' :
+                                'Order Item',
+                            'value' => [
+                                'items' => $order->orderItems()->mapToArray(
+                                    static function (OrderItem $item): array {
+                                        $content = $item->software()->name();
 
-                                            $content .= '<br>' . $item->priceFormatted();
+                                        $content .= '<br>' . $item->priceFormatted();
 
-                                            if ($item->quantity() > 1) {
-                                                $content .= ' x ' . $item->quantity();
-                                            }
+                                        if ($item->quantity() > 1) {
+                                            $content .= ' x ' . $item->quantity();
+                                        }
 
-                                            $content .= '</small>';
+                                        $content .= '</small>';
 
-                                            return [
-                                                'content' => $content,
-                                                'links' => [
-                                                    [
-                                                        'href' => $item->license()->accountLink(),
-                                                        'content' => 'License',
-                                                    ],
-                                                    [
-                                                        'href' => $item->software()->pageLink(),
-                                                        'content' => 'Software Page',
-                                                    ],
+                                        return [
+                                            'content' => $content,
+                                            'links' => [
+                                                [
+                                                    'href' => $item->license()->accountLink(),
+                                                    'content' => 'License',
                                                 ],
-                                            ];
-                                        },
-                                    ),
-                                ],
+                                                [
+                                                    'href' => $item->software()->pageLink(),
+                                                    'content' => 'Software Page',
+                                                ],
+                                            ],
+                                        ];
+                                    },
+                                ),
                             ],
                         ],
                     ],
                 ],
-            ),
-        );
+            ],
+        ));
 
         return $response;
     }
