c5b048fd2cd047dca301ed0fc9bf080a9deac01e
TJ Draper <tj@buzzingpixel.com>
2021-08-21 14:01:34 -0500

    Set up index all issues to run every day at midnight

---
+++ src/Context/ElasticSearch/Schedule/IndexAllIssuesSchedule.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Context\ElasticSearch\Schedule;
+
+use App\Context\ElasticSearch\QueueActions\IndexAllIssuesQueueAction;
+use App\Context\Queue\Entities\Queue;
+use App\Context\Queue\Entities\QueueItem;
+use App\Context\Queue\QueueApi;
+use App\Context\Schedule\Entities\ScheduleConfigItem;
+use App\Context\Schedule\Frequency;
+
+class IndexAllIssuesSchedule
+{
+    public static function getScheduleConfig(): ScheduleConfigItem
+    {
+        return new ScheduleConfigItem(
+            className: self::class,
+            runEvery: Frequency::DAY_AT_MIDNIGHT,
+        );
+    }
+
+    public function __construct(private QueueApi $queueApi)
+    {
+    }
+
+    public function __invoke(): void
+    {
+        $this->queueApi->addToQueue(
+            queue: (new Queue())
+                ->withHandle(handle: 'index-all-issues')
+                ->withAddedQueueItem(
+                    newQueueItem: new QueueItem(
+                        className: IndexAllIssuesQueueAction::class,
+                        methodName: 'sync',
+                    ),
+                ),
+        );
+    }
+}
