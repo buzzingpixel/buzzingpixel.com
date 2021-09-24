61ef9bf1183e7c4c50d52608765d3061ff6b3703
TJ Draper <tj@buzzingpixel.com>
2021-09-23 21:13:30 -0500

    Fix production utility script

--- docker/utility/prod.sh
+++ docker/utility/prod.sh
@@ -7,11 +7,7 @@ touch /log-volume/nginx/error.log;
 chmod -R 0777 /log-volume;
 
 while true; do
-    chmod -R 0777 /tmp;
-    sleep 5;
-done
-
-while true; do
     chmod -R 0777 /storage-volume;
+    chmod -R 0777 /tmp;
     sleep 120;
 done
