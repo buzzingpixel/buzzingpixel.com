84722adc59deb7ece5a19aea75c1f6638ffbd955
TJ Draper <tj@buzzingpixel.com>
2021-09-25 11:41:34 -0500

    Update queueRunner.sh script

--- queueRunner.sh
+++ queueRunner.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 while true; do
-    php /opt/project/cli queue:run --quiet;
+    /usr/bin/docker exec -w /opt/project buzzingpixel-app bash -c "XDEBUG_MODE=off php cli queue:run --quiet";
     sleep 1;
 done
