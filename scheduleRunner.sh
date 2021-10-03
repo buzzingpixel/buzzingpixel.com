5f2db02635603e4770c86a8fbacb925e95c2b79c
TJ Draper <tj@buzzingpixel.com>
2021-10-02 19:41:12 -0500

    Fix an issue with enable/disable schedule when deploying

--- scheduleRunner.sh
+++ scheduleRunner.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ -f "/root/buzzingpixel.com/disableSchedule" ]]; then
+if [[ ! -f "/root/buzzingpixel.com/disableSchedule" ]]; then
     /usr/bin/docker exec -w /opt/project buzzingpixel-app bash -c "XDEBUG_MODE=off php cli schedule:run --quiet";
 fi
