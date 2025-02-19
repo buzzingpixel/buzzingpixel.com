cb8fabe4043855e2b705bcb98f448ce2617f0e3e
TJ Draper <tj@buzzingpixel.com>
2025-02-18 22:55:53 -0600

    Remove things from the schedule we don't need anymore

--- config/ScheduleConfig.php
+++ config/ScheduleConfig.php
@@ -6,11 +6,11 @@ namespace Config;
 
 use App\Context\DatabaseCache\Schedule\PruneExpiredCache;
 use App\Context\ElasticSearch\Schedule\SetUpIndicesSchedule;
-use App\Context\Licenses\Schedule\UpdateMaxVersionOnLicensesSchedule;
+// use App\Context\Licenses\Schedule\UpdateMaxVersionOnLicensesSchedule;
 use App\Context\Queue\Schedule\CleanDeadQueues;
 use App\Context\Queue\Schedule\CleanOldQueues;
 use App\Context\Schedule\Entities\ScheduleConfigItemCollection;
-use App\Context\Stripe\Schedule\StripeSyncEveryNight;
+// use App\Context\Stripe\Schedule\StripeSyncEveryNight;
 use App\Context\TempFileStorage\Schedule\CleanUploadedFilesSchedule;
 use App\Context\Users\Schedule\UserResetTokenGarbageCollection;
 use App\Context\Users\Schedule\UserSessionGarbageCollection;
@@ -26,8 +26,8 @@ class ScheduleConfig
             CleanUploadedFilesSchedule::getScheduleConfig(),
             PruneExpiredCache::getScheduleConfig(),
             SetUpIndicesSchedule::getScheduleConfig(),
-            StripeSyncEveryNight::getScheduleConfig(),
-            UpdateMaxVersionOnLicensesSchedule::getScheduleConfig(),
+            // StripeSyncEveryNight::getScheduleConfig(),
+            // UpdateMaxVersionOnLicensesSchedule::getScheduleConfig(),
             UserResetTokenGarbageCollection::getScheduleConfig(),
             UserSessionGarbageCollection::getScheduleConfig(),
         ]);
